42fd4ddfe3d748c5d4b7e23654d0ed7439abdcf1
babuuGG <tamangsukraraj32@gmail.com>
2024-04-30 11:23:28 +0545

    third question

---
+++ Third_qn.java
@@ -0,0 +1,11 @@
+public class Third_qn {
+    public static void main(String[] args){
+        System.out.print("A \"quoted\" String is\n");
+        System.out.print("\'much\' better if yout learn \n");
+        System.out.print("the rules of \"escape sequence.\" \n");
+        System.out.print("Also,\"\" represents an empty \n");
+        System.out.print("String. Don't forget: use \\\" \n");
+        System.out.print("instead of \" !\n");
+        System.out.print("\" is not the same as \"");
+    }
+}
