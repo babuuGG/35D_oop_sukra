23add75d30deaf6af6636e61d5d9c43cba559ea8
babuuGG <tamangsukraraj32@gmail.com>
2024-04-30 11:25:19 +0545

    fifth question

---
+++ fifth_question.java
@@ -0,0 +1,13 @@
+public class fifth_question {
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 9;
+
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= columns; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
