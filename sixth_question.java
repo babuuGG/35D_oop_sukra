b09b40d25d4591c3743decb155990d9fe6cdd1e3
babuuGG <tamangsukraraj32@gmail.com>
2024-04-30 11:26:22 +0545

    sixth question

---
+++ sixth_question.java
@@ -0,0 +1,27 @@
+public class Sixth_question {
+    public static void main(String[] args){
+        System.out.println("A well-formed");
+        System.out.println("Java program");
+        System.out.println("has a main");
+        System.out.println("method with {");
+        System.out.println("and } braces.");
+        System.out.println("");
+        System.out.println("A");
+        System.out.println("System.out.pri");
+        System.out.println("ntln statement");
+        System.out.println("has (and) and");
+        System.out.println("usually a String");
+        System.out.println("that starts and");
+        System.out.println("ends with a \"");
+        System.out.println("character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("what is the difference between");
+        System.out.println("a ' and a \\\"? Or between a \\\" and a \\\\\\\"?");
+        System.out.println("");
+        System.out.println("one is what we see when");
+        System.out.println("we're typing our program. The");
+        System.out.println("other is what appears on the ");
+        System.out.println("\"console\"");
+
+    }
+}
