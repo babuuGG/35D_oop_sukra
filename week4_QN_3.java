a4d5f20fcd5bb9c566c2f1abed91feea01e424dc
babuuGG <tamangsukraraj32@gmail.com>
2024-05-15 15:41:35 +0545

    week 4 question no 3 is added

---
+++ week4_QN_3.java
@@ -0,0 +1,9 @@
+public class week4_QN_3 {
+    public static void main(String[] args) {
+    System.out.println("numbers square");
+    for(int number = 10;number >=1; number--){
+        int square = number * number;
+        System.out.printf("%-8d  %-6d\n",number,square);
+    }    
+    }
+}
